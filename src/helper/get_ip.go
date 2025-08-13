03b62e9f4dc4bd75befb260ba6926f4aa1d1fb61
Ponito <vvbdac58@gmail.com>
2025-08-13 16:58:29 +0800

    Adds printing the outbound ip for others to connect :D

---
+++ src/helper/get_ip.go
@@ -0,0 +1,16 @@
+package helper
+
+import "net"
+
+func GetOutboundIp() (ip net.IP, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	ip = localAddr.IP
+	return
+}
--- src/main.go
+++ src/main.go
@@ -4,19 +4,42 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"ponito/lilser/helper"
 )
 
+func printIp() {
+	var (
+		ip  net.IP
+		err error
+	)
+
+	ip, err = helper.GetOutboundIp()
+	if err != nil {
+		return
+	}
+
+	log.Println("Your outbound ip is", ip)
+}
+
 func main() {
-	port := "8080"
-	flag.StringVar(&port, "p", port, "port to listen on")
+	var (
+		port int
+		addr string
+	)
+
+	flag.IntVar(&port, "p", 8080, "the port to forward with")
 	flag.Parse()
 
 	fileServer := http.FileServer(http.Dir("."))
 	http.Handle("/", fileServer)
 
-	addr := fmt.Sprintf("0.0.0.0:%s", port)
+	addr = fmt.Sprintf("0.0.0.0:%d", port)
 	log.Printf("Serving files in %s", addr)
+
+	go printIp()
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal(err)
 	}
