f5c688820b9075381f9d88aaec52e72fccb8577a
Ponito <vvbdac58@gmail.com>
2025-08-17 00:32:11 +0800

    Refactored file upload API and removed unused HTML file upload page

--- src/apis/file_api.go
+++ src/apis/file_api.go
@@ -1,7 +1,7 @@
 package apis
 
 import (
-	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 	"ponito/lilser/apis/helpers"
 	"ponito/lilser/apis/helpers/urling"
+	"sort"
 )
 
 func printWorkingDir() {
@@ -38,7 +39,6 @@ var putFile = func(w http.ResponseWriter, r *http.Request) (state int, err error
 		queries  urling.QueryKvps
 		folder   string
 		filename string
-		dirInfo  os.FileInfo
 		tmpFile  *os.File
 	)
 
@@ -64,7 +64,7 @@ var putFile = func(w http.ResponseWriter, r *http.Request) (state int, err error
 	{
 		filename = queries.Get(FileName)[0]
 		folder = filepath.Dir(filename)
-		if dirInfo, err = os.Stat(folder); err != nil {
+		if _, err = os.Stat(folder); err != nil {
 			if os.IsNotExist(err) {
 				if err = os.MkdirAll(folder, 0755); err != nil {
 					state = 500
@@ -86,19 +86,26 @@ var putFile = func(w http.ResponseWriter, r *http.Request) (state int, err error
 
 	// atomic file replacing
 	{
-		if tmpFile, err = os.CreateTemp(dirInfo.Name(), "tmp"); err != nil {
+		if tmpFile, err = os.CreateTemp(folder, "tmp-*"); err != nil {
 			state = 500
 			return
 		}
-		defer tmpFile.Close()
 
 		if _, err = io.Copy(tmpFile, r.Body); err != nil {
+			_ = tmpFile.Close()
 			state = 500
 			return
 		}
 
-		err = os.Rename(tmpFile.Name(), filename)
-		log.Println(tmpFile.Name(), filename, err)
+		if err = tmpFile.Close(); err != nil {
+			state = 500
+			return
+		}
+
+		if err = os.Rename(tmpFile.Name(), filename); err != nil {
+			state = 500
+			return
+		}
 	}
 	return
 }
@@ -151,49 +158,27 @@ var getFile = func(w http.ResponseWriter, r *http.Request) (state int, err error
 			return
 		}
 
+		// build view model and sort entries by name
+		type dirEntryView struct {
+			Name  string
+			IsDir bool
+		}
+		views := make([]dirEntryView, 0, len(entries))
+		for _, e := range entries {
+			views = append(views, dirEntryView{Name: e.Name(), IsDir: e.IsDir()})
+		}
+		sort.SliceStable(views, func(i, j int) bool { return views[i].Name < views[j].Name })
+
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-		for _, entry := range entries {
-			var msg string
-			msg = fmt.Sprintf("<a href='?filename=%s/%s'>%s</a><br>", filename, entry.Name(), entry.Name())
-			_, _ = w.Write([]byte(msg))
-		}
-
-		_, _ = w.Write([]byte(fmt.Sprintf(`
-<input id='file' type="file" name="file" required/>
-<script>
-    function putFile() {
-        const file = document.querySelector('input[type="file"]').files[0];
-        fetch('?filename=%s/' + file.name, {
-            method: 'PUT',
-            body: file,
-        })
-        .then(rsp => rsp.json())
-        .then(_ => window.location.reload())
-        .catch(err => console.log(err))
-    }
-
-    document.getElementById('file').onchange = e => {
-        putFile();
-    }
-</script>
-`, filename)))
+		_ = dirListTmpl.Execute(w, struct {
+			Path    string
+			Entries []dirEntryView
+		}{
+			Path:    filename,
+			Entries: views,
+		})
 	} else {
-		var (
-			file *os.File
-		)
-
-		if file, err = os.Open(filename); err != nil {
-			state = 500
-			return
-		}
-
-		defer file.Close()
-
-		if _, err = io.Copy(w, file); err != nil {
-			state = 500
-			return
-		}
+		http.ServeFile(w, r, filename)
 	}
 	return
 }
@@ -204,3 +189,43 @@ func UseFile() {
 	http.HandleFunc("PUT /file", helpers.Wrap(putFile))
 	http.HandleFunc("GET /file", helpers.Wrap(getFile))
 }
+
+var dirListTmpl = template.Must(template.New("dirlist").Parse(`<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Index of {{.Path}}</title>
+  <style>
+    body { font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif; padding: 16px; }
+    ul { list-style: none; padding-left: 0; }
+    li { margin: 4px 0; }
+    a { text-decoration: none; }
+  </style>
+</head>
+<body>
+  <h3>Index of {{.Path}}</h3>
+  <ul>
+    {{range .Entries}}
+      <li><a href="?filename={{$.Path}}/{{.Name}}">{{.Name}}{{if .IsDir}}/{{end}}</a></li>
+    {{end}}
+  </ul>
+  <div>
+    <input id="file" type="file" name="file" required />
+  </div>
+  <script>
+    (function(){
+      var input = document.getElementById('file');
+      input.addEventListener('change', function(){
+        var f = input.files[0];
+        if(!f) return;
+        var p = '{{.Path}}';
+        var url = '?filename=' + encodeURIComponent(p) + '/' + encodeURIComponent(f.name);
+        fetch(url, { method: 'PUT', body: f })
+          .then(function(r){ if(!r.ok) throw new Error(r.statusText); })
+          .then(function(){ window.location.reload(); })
+          .catch(function(e){ console.log(e); alert('Upload failed: ' + e); });
+      });
+    })();
+  </script>
+</body>
+</html>`))
