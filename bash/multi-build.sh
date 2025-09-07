29c12c0d089b1669b1d22caf8fecafccf1418d7d
Ponito <vvbdac58@gmail.com>
2025-09-07 15:57:22 +0800

    update building args

--- bash/multi-build.sh
+++ bash/multi-build.sh
@@ -3,6 +3,6 @@
 cd ../src
 
 go build -o ./bin/lilser
-GOOS=windows GOARCH=amd64 go build -o ./bin/windows/lilser.exe
-GOOS=linux GOARCH=amd64 go build -o ./bin/linux/lilser
-GOOS=darwin GOARCH=arm64 go build -o ./bin/mac/lilser
+GOOS=windows GOARCH=amd64 go build -o ./bin/windows-amd-lilser.exe
+GOOS=linux GOARCH=amd64 go build -o ./bin/linux-amd-lilser
+GOOS=darwin GOARCH=arm64 go build -o ./bin/mac-arm-lilser
